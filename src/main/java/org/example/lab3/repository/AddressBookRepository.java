274ee5b5cd6828454950689a7e199994190c76e2
Hasib <khodayarhasib@gmail.com>
2025-10-04 19:03:56 -0400

    initial commit

---
+++ src/main/java/org/example/lab3/repository/AddressBookRepository.java
@@ -0,0 +1,7 @@
+package org.example.lab3.repository;
+import org.example.lab3.entities.AddressBook;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressBookRepository extends CrudRepository<AddressBook, Long>{
+
+}
---
+++ src/main/java/org/example/lab3/repository/BuddyInfoRepository.java
@@ -0,0 +1,8 @@
+package org.example.lab3.repository;
+
+import org.example.lab3.entities.BuddyInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BuddyInfoRepository extends CrudRepository<BuddyInfo, Long> {
+
+}
